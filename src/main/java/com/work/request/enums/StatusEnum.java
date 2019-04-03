feb3a7fd7a1e5c3aeae60a183361fadbe828fa48
Sergey Ivanov <sergeiivanov01@gmail.com>
2019-04-03 18:14:55 +0800

    Заявки

--- src/main/java/com/work/request/enums/StatusEnum.java
+++ src/main/java/com/work/request/enums/StatusEnum.java
@@ -4,14 +4,14 @@ public enum StatusEnum {
     NEW("Новая"),
     WORK("В работе"),
     RESOLVED("Решена"),
-    REJECTED("Отклонена");
+    REJECTED("Отклонена1");
 
     private String status;
 
     StatusEnum(String status) {
     }
 
-    public final String getStatus( ) {
+    public final String getStatus() {
         return status;
     }
 }
